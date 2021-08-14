7c21162e24fd327316958edab3937ed558a9b8f2
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2021-08-13 19:35:38 -0700

    [init] Initial commit to project

---
+++ HopSkipDrive-Take-Home-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HopSkipDrive-Take-Home-Project
+//
+//  Created by Ryan Nguyen on 8/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
