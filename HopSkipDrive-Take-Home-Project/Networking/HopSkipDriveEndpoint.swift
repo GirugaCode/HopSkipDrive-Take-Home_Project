7e7e40c43373c9ce8f1f4f361af832cdfc5a4d89
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2021-08-16 09:58:55 -1000

    [add] HopSkipDrive Endpoint

---
+++ HopSkipDrive-Take-Home-Project/Networking/HopSkipDriveEndpoint.swift
@@ -0,0 +1,46 @@
+//
+//  HopSkipDriveEndpoint.swift
+//  HopSkipDrive-Take-Home-Project
+//
+//  Created by Ryan Nguyen on 8/16/21.
+//
+
+import Foundation
+
+enum HopSkipDriveEndpoint: Endpoint {
+    var scheme: String {
+        switch self {
+        default:
+            return "https"
+        }
+    }
+    
+    var baseURL: String {
+        switch self {
+        default:
+            return "https://storage.googleapis.com/"
+        }
+    }
+    
+    var path: String {
+        switch self {
+        default:
+            return "/hsd-interview-resources/simplified_my_rides_response.json/"
+        }
+    }
+    
+    var parameters: [URLQueryItem] {
+        switch self {
+        default:
+            return []
+        }
+    }
+    
+    var method: String {
+        switch self {
+        default:
+            return "GET"
+        }
+    }
+
+}
